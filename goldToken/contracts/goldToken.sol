0054cf1192dabbb6355d19b74b1937acc0771f5c
AADARSH SINGH <95958161+HuLkBUStEr007@users.noreply.github.com>
2023-10-10 01:37:04 +0530

    Add files via upload

---
+++ goldToken/contracts/goldToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract goldToken is ERC20, ERC20Permit {
+    constructor() ERC20("goldToken", "MTK") ERC20Permit("goldToken") {}
+
+    function mint(address to,uint256 amount )public{
+        _mint(to,amount);
+
+       }
+
+       function burn(address from,uint256 amount)public{
+        _burn(from,amount);
+}
+}
