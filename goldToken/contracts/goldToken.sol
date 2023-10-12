8700283de45ab397847617b322fd248e70100c7b
AADARSH SINGH <95958161+HuLkBUStEr007@users.noreply.github.com>
2023-10-12 23:05:29 +0530

    Update goldToken.sol

--- goldToken/contracts/goldToken.sol
+++ goldToken/contracts/goldToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract goldToken is ERC20, ERC20Permit {
-    constructor() ERC20("goldToken", "MTK") ERC20Permit("goldToken") {}
+    constructor() ERC20("goldToken", "GT") ERC20Permit("goldToken") {}
 
     function mint(address to,uint256 amount )public{
         _mint(to,amount);
